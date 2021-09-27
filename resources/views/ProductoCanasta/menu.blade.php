6fd2828c8a0ce531cd8dd5c49df309c64024edc6
Nisos <josess.nicolas@hotmail.com>
2021-09-27 00:13:38 -0500

    Update menu.blade.php

--- resources/views/ProductoCanasta/menu.blade.php
+++ resources/views/ProductoCanasta/menu.blade.php
@@ -1,150 +1,155 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>#CaucaAgro</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>#CaucaAgro</title>
 
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
-	<script src="https://use.fontawesome.com/releases/v5.0.8/js/all.js"></script>
-	<link href="style.css" rel="stylesheet">
-	<link rel="stylesheet" href="https://m.w3newbie.com/you-tube.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+    <script src="https://use.fontawesome.com/releases/v5.0.8/js/all.js"></script>
+    <link href="style.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://m.w3newbie.com/you-tube.css">
 </head>
 <body>
 <!-- Navigation -->
 
-	<nav class="navbar navbar-expand-md navbar-light bg-light sticky-top">
-		<div class="container-fluid">
-			<a class="navbar-brand" href="#"><img src="img/logo.png"></a>
-			<button class="navbar-toggler" type="button" data-toggle="collapse"
-			data-target="#navbarResponsive">
-				<span class="navbar-toggler-icon"></span>
-			</button>
-		<div class="collapse navbar-collapse" id="navbarResponsive">
-			<ul class="navbar-nav ml-auto">
-				<li class="nav-item active">
-					<a class="nav-link" href="{{route('ProductoCanasta.index') }}">Canasta agrícola</a>
-				</li>
-				<li class="nav-item">
-					<a class="nav-link" href="#">Emprendiminetos y asociaciones</a>
-				</li>
-				<li class="nav-item">
-					<a class="nav-link" href="#">Agrooferta</a>
-				</li>
-				<li class="nav-item">
-					<a class="nav-link" href="#gente">Inversionistas</a>
-				</li>
-				<li class="nav-item">
-					<a class="nav-link" href="#evento">Eventos</a>
-				</li>
-				<li class="nav-item">
-				@guest
-                            @if (Route::has('login'))
-                                <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                                </li>
-                            @endif
+<nav class="navbar navbar-expand-md navbar-light bg-light sticky-top">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#"><img src="img/logo.png"></a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse"
+                data-target="#navbarResponsive">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="{{route('ProductoCanasta.index') }}">Canasta agrícola</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Emprendiminetos y asociaciones</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Agrooferta</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#gente">Inversionistas</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#evento">Eventos</a>
+                </li>
+                <li class="nav-item">
+            </ul>
+            <ul class="navbar-nav ml-auto">
+                @guest
+                    @if (Route::has('login'))
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                        </li>
+                    @endif
 
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }}
-                                </a>
+                    @if (Route::has('register'))
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                        </li>
+                    @endif
+                @else
+                    <li class="nav-item dropdown">
+                        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
+                           data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                            {{ Auth::user()->name }}
+                        </a>
 
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
+                        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="{{ route('logout') }}"
+                               onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
+                                {{ __('Logout') }}
+                            </a>
 
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
-				</li>
-			</ul>
-		</div>
-		</div>
-	</nav>
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                @csrf
+                            </form>
+                        </div>
+                    </li>
+                @endguest
+            </ul>
+        </div>
+    </div>
+</nav>
 
 <!--- Image Slider -->
 <div id="slides" class="carousel slide" data-ride="carousel">
-	<ul class="carousel-indicators">
-		<li data-target="#slides" data-slide-to="0" class="active"></li>
-		<li data-target="#slides" data-slide-to="1"></li>
-		<li data-target="#slides" data-slide-to="2"></li>
-	</ul>
-	<div class="carousel-inner">
-		<div class="carousel-item active">
-			<img src="img/background.jpg" >
-			<div class="carousel-caption">
-				<h2 class="display-2">Visita nuestra tienda virtual</h2>
-				<button type="button" class="btn btn-outline-light btn-lg">Ir a la Tienda</button>
-				<button type="button" class="btn btn-primary btn-lg">Agrooferta</button>
-			</div>
-		</div>
-		<div class="carousel-item">
-			<img src="img/background2.jpg">
-		</div>
+    <ul class="carousel-indicators">
+        <li data-target="#slides" data-slide-to="0" class="active"></li>
+        <li data-target="#slides" data-slide-to="1"></li>
+        <li data-target="#slides" data-slide-to="2"></li>
+    </ul>
+    <div class="carousel-inner">
+        <div class="carousel-item active">
+            <img src="img/background.jpg">
+            <div class="carousel-caption">
+                <h2 class="display-2">Visita nuestra tienda virtual</h2>
+                <button type="button" class="btn btn-outline-light btn-lg">Ir a la Tienda</button>
+                <button type="button" class="btn btn-primary btn-lg">Agrooferta</button>
+            </div>
+        </div>
+        <div class="carousel-item">
+            <img src="img/background2.jpg">
+        </div>
         <div class="carousel-item">
             <img src="img/background3.jpg">
         </div>
-	</div>
+    </div>
 </div>
 
 <!--- Jumbotron -->
 <section id="jumbotron">
-	<div class="container-fluid">
-		<div class="row jumbotron">
-			<div class="col-xs-12 col-sm-12 col-md-9 col-lg-9 col-xl-10">
-				<p class="lead">Apoya el campo caucano comprando en nuestra tienda virtual
-				</p>
-			</div>
-			<div class="col-xs-12 col-sm-12 col-md-3 col-lg-3 col-xl-2">
-				<a href="#"><button type="button" class="btn btn-outline-secondary
-					btn-lg">TIENDA </button></a>
-			</div>
-		</div>
-	</div>
+    <div class="container-fluid">
+        <div class="row jumbotron">
+            <div class="col-xs-12 col-sm-12 col-md-9 col-lg-9 col-xl-10">
+                <p class="lead">Apoya el campo caucano comprando en nuestra tienda virtual
+                </p>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-3 col-lg-3 col-xl-2">
+                <a href="#">
+                    <button type="button" class="btn btn-outline-secondary
+					btn-lg">TIENDA
+                    </button>
+                </a>
+            </div>
+        </div>
+    </div>
 </section>
-
 
 
 <!--- Two Column Section -->
 <div class="container-fluid padding">
-	<div class="row padding">
-		<div class="col-lg-6">
-			<h2>Emprendiminetos y asociaciones</h2>
-			<p>Uno de los objetivos de cauca agrosostenible es promover los emprendimientos de nuestro departamento
-				por lo cual decimos darle todo un espacio para que cada persona se tome el trabajo de conocerlas mas a fondo
+    <div class="row padding">
+        <div class="col-lg-6">
+            <h2>Emprendiminetos y asociaciones</h2>
+            <p>Uno de los objetivos de cauca agrosostenible es promover los emprendimientos de nuestro departamento
+                por lo cual decimos darle todo un espacio para que cada persona se tome el trabajo de conocerlas mas a
+                fondo
 
-			</p>
-			 <br>
-			 <a href="#" class="btn btn-primary">Mas y mas</a>
-		</div>
-		<div class="col-lg-6">
-			<img src="img/emprendedores.jpg" class="img-fluid">
-		</div>
-	</div>
+            </p>
+            <br>
+            <a href="#" class="btn btn-primary">Mas y mas</a>
+        </div>
+        <div class="col-lg-6">
+            <img src="img/emprendedores.jpg" class="img-fluid">
+        </div>
+    </div>
 </div>
 
 <hr class="my-4">
 <!--- Fixed background -->
 <figure>
-	<div class="fixed-wrap">
-		<div id="fixed"></div>
-	</div>
+    <div class="fixed-wrap">
+        <div id="fixed"></div>
+    </div>
 </figure>
 
 <!--- Emoji Section -->
@@ -152,94 +157,81 @@
 
 <!--- Meet the team -->
 <section id="gente">
-	<div class="container-fluid padding">
-		<div class="row welcome text-center">
-			<div class="col-12">
-				<h1 class="display-4">Inversionistas </h1>
-			</div>
-			<hr>
-		</div>
-	</div>
+    <div class="container-fluid padding">
+        <div class="row welcome text-center">
+            <div class="col-12">
+                <h1 class="display-4">Inversionistas </h1>
+            </div>
+            <hr>
+        </div>
+    </div>
 </section>
 
 <!--- Cards -->
 <div class="container-fluid padding">
-	<div class="row padding">
-		<div class="col-md-4">
-			<div class="card">
-				<img class="card-img-top" src="img/team1.png">
-				<div class="card-body">
-					<h4 class="card-title">Inversionista 1</h4>
-					<p class="card-text">Informacion que nadie ve sobre esta persona</p>
-					<a href="#" class="btn btn-outline-secondary">Ver mas info</a>
-				</div>
-			</div>
-		</div>
+    <div class="row padding">
+        <div class="col-md-4">
+            <div class="card">
+                <img class="card-img-top" src="img/team1.png">
+                <div class="card-body">
+                    <h4 class="card-title">Inversionista 1</h4>
+                    <p class="card-text">Informacion que nadie ve sobre esta persona</p>
+                    <a href="#" class="btn btn-outline-secondary">Ver mas info</a>
+                </div>
+            </div>
+        </div>
 
-		<div class="col-md-4">
-			<div class="card">
-				<img class="card-img-top" src="img/team2.png">
-				<div class="card-body">
-					<h4 class="card-title">Inversionista 2</h4>
-					<p class="card-text">Informacion que nadie ve sobre esta persona</p>
-					<a href="#" class="btn btn-outline-secondary">Ver mas info</a>
-				</div>
-			</div>
-		</div>
+        <div class="col-md-4">
+            <div class="card">
+                <img class="card-img-top" src="img/team2.png">
+                <div class="card-body">
+                    <h4 class="card-title">Inversionista 2</h4>
+                    <p class="card-text">Informacion que nadie ve sobre esta persona</p>
+                    <a href="#" class="btn btn-outline-secondary">Ver mas info</a>
+                </div>
+            </div>
+        </div>
 
-		<div class="col-md-4">
-			<div class="card">
-				<img class="card-img-top" src="img/team3.png">
-				<div class="card-body">
-					<h4 class="card-title">Inversionista 3</h4>
-					<p class="card-text">Informacion que nadie ve sobre esta persona</p>
-					<a href="#" class="btn btn-outline-secondary">Ver mas info</a>
-				</div>
-			</div>
-		</div>
-	</div>
+        <div class="col-md-4">
+            <div class="card">
+                <img class="card-img-top" src="img/team3.png">
+                <div class="card-body">
+                    <h4 class="card-title">Inversionista 3</h4>
+                    <p class="card-text">Informacion que nadie ve sobre esta persona</p>
+                    <a href="#" class="btn btn-outline-secondary">Ver mas info</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<hr class="my-4">
 
-	<!--- Welcome Section -->
+<!--- Welcome Section -->
 <section id="evento">
-<div class="container-fluid padding">
-	<div class="row welcome text-center">
-		<div class="col-12">
-			<h1 class="display-4">Eventos</h1>
-		</div>
-		<hr>
-		<div class="col-12">
-			<p class="lead">Conoce los eventos mas cercanos en el cauca </p>
-		</div>
-	</div>
-</div>
+    <div class="container-fluid padding">
+        <div class="row welcome text-center">
+            <div class="col-12">
+                <h1 class="display-4">Eventos</h1>
+            </div>
+            <hr>
+            <div class="col-12">
+                <p class="lead">Conoce los eventos mas cercanos en el cauca </p>
+            </div>
+        </div>
+    </div>
 
-<!--- Three Column Section -->
-<div class="container-fluid padding">
-	<div class="row text-center padding">
+    <!--- Three Column Section -->
+    <div class="container-fluid padding">
+        <div class="row text-center padding">
 
-		<div class="col-sm-12 col-md-4">
-		</div>
-		<iframe src="https://www.google.com/maps?q=Popayan%20Cauca%2C%20Carrera%2016%20Norte%2C%20Popay%C3%A1n%2C%20Cauca%2C%20Colombia&z=12&t=&ie=UTF8&output=embed"  frameborder="0" style="border:0; width: 100%; height: 290px;" allowfullscreen></iframe>
-		</div>
-		</div>
-	</div>
-	<hr class="my-4">
-</div>
+            <div class="col-sm-12 col-md-4">
+            </div>
+            <iframe
+                src="https://www.google.com/maps?q=Popayan%20Cauca%2C%20Carrera%2016%20Norte%2C%20Popay%C3%A1n%2C%20Cauca%2C%20Colombia&z=12&t=&ie=UTF8&output=embed"
+                frameborder="0" style="border:0; width: 100%; height: 290px;" allowfullscreen></iframe>
+        </div>
+    </div>
 </section>
-
-
-</div>
-
-<!--- Two Column Section -->
-
-
-<!--- Connect -->
-
-
-<!--- Footer -->
-
-
-
 
 </body>
 </html>
